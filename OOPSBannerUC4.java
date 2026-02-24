2d3cbf493213a8e112f8148d209bd3130475de97
Manoranjan.G <ushimanoranjan@gmail.com>
2026-02-24 13:01:06 +0530

    Add files via upload

---
+++ OOPSBannerUC4.java
@@ -0,0 +1,17 @@
+public class OOPSBannerUC4 {
+    public static void main(String[] args) {
+        String[] lines = {
+            String.join(" ", "******", "******", "******", "******"),
+            String.join(" ", "**  **", "**  **", "**  **", "**    "),
+            String.join(" ", "**  **", "**  **", "**  **", "**    "),
+            String.join(" ", "**  **", "**  **", "******", "******"),
+            String.join(" ", "**  **", "**  **", "**     ", "    **"),
+            String.join(" ", "**  **", "**  **", "**     ", "    **"),
+            String.join(" ", "******", "******", "**     ", "******")
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
