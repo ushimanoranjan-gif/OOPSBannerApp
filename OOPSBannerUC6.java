c201a2b9df3c7092654e2618f3823674ad6b9971
ushimanoranjan-gif <ushimanoranjan@gmail.com>
2026-03-02 12:29:24 +0530

    UC6

---
+++ OOPSBannerUC6.java
@@ -0,0 +1,7 @@
+public class OOPSBannerUC6 {
+    public static void main(String[] args) {
+	characterPatternmap[]charMaps =createCharacterPatternMaps();
+	String message ="OOPS";
+	printMessage(message,charMaps);
+	}
+}
